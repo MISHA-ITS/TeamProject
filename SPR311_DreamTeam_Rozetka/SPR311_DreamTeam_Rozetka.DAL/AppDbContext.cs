c0a7d2343a7498eb00e17bb74bfb54ff628133e8
MISHA-ITS <mybox018@gmail.com>
2025-08-24 22:41:51 +0300

    Створив AppDbContext

---
+++ SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka.DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPR311_DreamTeam_Rozetka.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
--- SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka/Program.cs
+++ SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka/Program.cs
@@ -1,17 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using SPR311_DreamTeam_Rozetka.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql("name=defaultdb");
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
