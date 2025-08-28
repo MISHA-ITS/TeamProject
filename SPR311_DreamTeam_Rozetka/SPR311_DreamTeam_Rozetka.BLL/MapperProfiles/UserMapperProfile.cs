469e62923e16af78235c483484f9489f0c706024
MISHA-ITS <mybox018@gmail.com>
2025-08-28 23:26:16 +0300

    Add and setting identity automapper

---
+++ SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka.BLL/MapperProfiles/UserMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using SPR311_DreamTeam_Rozetka.BLL.DTOs.User;
+using SPR311_DreamTeam_Rozetka.DAL.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPR311_DreamTeam_Rozetka.BLL.MapperProfiles
+{
+    class UserMapperProfile : Profile
+    {
+        public UserMapperProfile()
+        {
+            //CreateUserDTO -> AppUser
+            CreateMap<CreateUserDTO, AppUser>()
+                .ForMember(dest => dest.Image, opt => opt.Ignore());
+
+            //UpdateUserDTO -> AppUser
+            CreateMap<UpdateUserDTO, AppUser>()
+                .ForMember(dest => dest.Image, opt => opt.Ignore());
+
+            //UserEntity -> CategoryDTO
+            CreateMap<AppUser, UserDTO>();
+        }
+    }
+}
