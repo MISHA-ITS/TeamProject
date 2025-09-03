201237423578eaff133bfa93f4591eed4870839c
loki00345 <99328513+loki00345@users.noreply.github.com>
2025-09-03 19:17:53 +0300

    Category DTOs

---
+++ SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka.BLL/DTOs/Category/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SPR311_DreamTeam_Rozetka.BLL.DTOs.Category
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int ProductsCount { get; set; }
+    }
+}
---
+++ SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka.BLL/Validators/Category/CreateValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SPR311_DreamTeam_Rozetka.BLL.DTOs.Category;
+
+namespace SPR311_DreamTeam_Rozetka.BLL.Validators.Category
+{
+    public class CreateValidator : AbstractValidator<CreateCategoryDto>
+    {
+        public CreateValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Назва категорії є обов'язковою")
+                .Length(2, 100).WithMessage("Назва повинна містити від 2 до 100 символів")
+                .Matches(@"^[a-zA-Zа-яА-ЯїЇіІєЄґҐ0-9\s\-\'""()&.,!?]+$")
+                .WithMessage("Назва містить недопустимі символи");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Опис не може перевищувати 500 символів")
+                .When(x => !string.IsNullOrEmpty(x.Description));
+        }
+    }
+}
