356ab88fc17612158750db2cb5a4c7b5e3d994f9
DudiyMaksim <maksdudiya25@gmail.com>
2025-09-03 14:51:22 +0200

    Add CategoryController

---
+++ SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPR311_DreamTeam_Rozetka.BLL.DTOs.Category;
+using SPR311_DreamTeam_Rozetka.BLL.Services.Category;
+
+namespace SPR311_DreamTeam_Rozetka.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateCategoryDto dto)
+        {
+            var response = await _categoryService.CreateAsync(dto);
+            return response.IsSuccess ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var response = await _categoryService.DeleteAsync(id);
+            return response.IsSuccess ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(UpdateCategoryDto dto)
+        {
+            var response = await _categoryService.UpdateAsync(dto);
+            return response.IsSuccess ? Ok(response) : BadRequest(response);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var response = await _categoryService.GetAllAsync();
+            return response.IsSuccess ? Ok(response) : BadRequest(response);
+        }
+    }
+}
