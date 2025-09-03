4509735a88b08a250036b9639e854d9b7ec8a48b
DudiyMaksim <maksdudiya25@gmail.com>
2025-09-03 14:31:47 +0200

    add CategoryService + some DTOs

---
+++ SPR311_DreamTeam_Rozetka/SPR311_DreamTeam_Rozetka.BLL/Services/Category/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using SPR311_DreamTeam_Rozetka.BLL.DTOs.Category;
+
+namespace SPR311_DreamTeam_Rozetka.BLL.Services.Category
+{
+    public interface ICategoryService
+    {
+        Task<ServiceResponse> CreateAsync(CreateCategoryDto dto);
+        Task<ServiceResponse> UpdateAsync(UpdateCategoryDto dto);
+        Task<ServiceResponse> DeleteAsync(Guid id);
+        Task<ServiceResponse> GetByIdAsync(Guid id);
+        Task<ServiceResponse> GetAllAsync();
+    }
+}
